42b5b34d005a7e930e892cc6b0006d2aa1706caf
yehorrudytsia <yehorrudytsia@gmail.com>
2022-01-14 20:03:51 +0200

    Implement transfer tokens test

--- test/token.js
+++ test/token.js
@@ -7,28 +7,34 @@ describe("Token contract", function () {
     let Token;
     let token;
     let owner;
+    let halfOfSupply;
 
     this.beforeEach(async function () {
-        [owner] = await ethers.getSigners();
+        [owner, address1, address2] = await ethers.getSigners();
         Token = await ethers.getContractFactory("Token");
-    
         token = await Token.deploy();
-  
         await token.deployed();
+
+        halfOfSupply = await token.totalSupply() / 2;
     })
 
     
     describe("Deployment", function () {
         it("Deployment should assign the half of the total supply of tokens to the owner", async function () {
             const ownerBalance = await token.getBalance(owner.address);
-            const halfOfSupply = await token.totalSupply() / 2;
             expect(halfOfSupply).to.equal(ownerBalance);
         });
     
         it("Deployment should lock the half of the total supply in special contract", async function () {
-            const halfOfSupply = await token.totalSupply() / 2;
             const valueLocked = await token.valueLocked()
             expect(halfOfSupply).to.equal(valueLocked);
+        });
+    })
+
+    describe("Transaction", function () {
+        it("Transaction should transfer tokens between accounts", async function() {
+            await token.transfer(await address1.getAddress(), 100);
+            expect(await token.getBalance(await address1.getAddress())).to.equal(100);
         });
     })
 });
